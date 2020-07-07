7453b352f565d0170de0a9ace32e2e836cf4d98e
Jhune Carlo Trogelio <jhunecarlotrogelio@gmail.com>
2020-07-07 14:57:28 +0800

    initial structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from 'App'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { Provider } from 'react-redux'
+import store from 'store/store'
+import * as serviceWorker from 'serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import { combineReducers } from 'redux'
+import { fork, all } from 'redux-saga/effects'
+
+/** 
+ *  ADD HERE THE ACTIONS/ REDUCERS 
+ * 
+ * */
+
+import { reducer as thunkReducer } from 'redux-saga-thunk'
+
+/** 
+ *  ADD HERE THE SAGAS 
+ * 
+ * */
+
+export const rootReducer = combineReducers({
+    thunk: thunkReducer,
+    /**
+     * ADD THE REDUCER COMPONENT 
+     */
+})
+
+export function* rootSaga() {
+    yield all([
+        
+    ].map(fork))
+}
+
