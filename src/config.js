9d3046e5d2a7c0fc52fade48e88cf2a9da89c4b9
postnzt <jhunecarlo.trogelio@gmail.com>
2020-07-13 19:57:31 +0000

    initialized the blackbox for the config file

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+  POW_HOST: process.env.REACT_APP_POW_HOST,
+}
+
+export default config
---
+++ src/store/create/sagas.js
@@ -0,0 +1,5 @@
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+
+import {
+  CREATE_FFS_REQUEST
+} from './actions'
