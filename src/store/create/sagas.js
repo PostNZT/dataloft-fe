a09b60cc0b32d82c9ed4088c872b9fe94ae086e5
postnzt <jhunecarlo.trogelio@gmail.com>
2020-07-14 21:51:54 +0000

    updated sagas for token in powergate

--- src/store/create/sagas.js
+++ src/store/create/sagas.js
@@ -15,25 +15,31 @@ import {
 function* createFFSRequest(payload, meta) {
   const POW_HOST = config.POW_HOST
   const powergate = createPow( { POW_HOST })
-
-
+  // console.log(POW_HOST)
+  // console.log(powergate)
   const { address } = payload
   const user = yield call(getWalletAccount, address)
-  console.log(user)
+  // console.log(user)
   if(user.status === 404) {
-    const { token } = powergate.ffs.create()
-    powergate.setToken(token)
-    const user = {
-      _id: address,
-      address,
-      token: token,
+    console.log('test')
+    try{
+      const { token } = yield call(powergate.ffs.create)
+      console.log(token)
+    } catch(e) {
+      console.log(e)
     }
-    const data = yield call(createWalletAccount, user)
+    // const user = {
+    //   _id: address,
+    //   address,
+    //   token: token,
+    // }
+
+    // const data = yield call(createWalletAccount, user)
     
   } 
   
 
-  TOKEN = token
+  // TOKEN = token
 
 }
 
