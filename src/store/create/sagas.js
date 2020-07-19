29ce5d095a7332d12cca81ae813d33f716866f09
postnzt <jhunecarlo.trogelio@gmail.com>
2020-07-20 02:34:38 +1400

    added sagas for create jwt token

--- src/store/create/sagas.js
+++ src/store/create/sagas.js
@@ -4,11 +4,13 @@ import {
   CREATE_FFS_REQUEST,
   createFFSSuccess,
   createFFSFailure,
+  CREATE_WALLET_JWT_TOKEN_REQUEST
 
 } from './actions'
 
 import {
   createFFS,
+  createWalletJWTToken,
 } from 'services/api'
 
 function* createFFSRequest(payload, meta) {
@@ -26,10 +28,21 @@ function* createFFSRequest(payload, meta) {
   }
 }
 
-function* watchCreateFFSRequest({ payload, meta}) {
+function* createWalletJWTTokenRequest(payload, meta) {
+  const { username, password, address, token } = payload
+  const data = yield call(createWalletJWTToken, username, password, address, token)
+  console.log(data)
+}
+
+function* watchCreateFFSRequest({ payload, meta }) {
   yield call(createFFSRequest, payload, meta)
 }
 
+function* watchCreateWalletJWTTokenRequest({ payload, meta }) {
+  yield call(createWalletJWTTokenRequest, payload, meta)
+}
+
 export default function* sagas() {
-  yield takeEvery(CREATE_FFS_REQUEST, watchCreateFFSRequest)
+  yield takeEvery(CREATE_FFS_REQUEST, watchCreateFFSRequest),
+  yield takeEvery(CREATE_WALLET_JWT_TOKEN_REQUEST, watchCreateWalletJWTTokenRequest)
 }
